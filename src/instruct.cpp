a5afb794dbdea06ba338f878768b08b21135a159
Jared Bruni <jaredbruni@gmail.com>
2025-07-24 14:01:08 -0700

    output format

--- src/instruct.cpp
+++ src/instruct.cpp
@@ -11,17 +11,20 @@ std::ostream &operator<<(std::ostream &out, const enum  Inc &i) {
 }
 
 std::ostream &operator<<(std::ostream &out, const mxvm::Instruction &inc) {
-    out << "[" << inc.instruction << "] ";
+    out << "[" << inc.instruction;
     std::ostringstream opc;
-    if (!inc.op1.op.empty()) opc << inc.op1.op;
+    if (!inc.op1.op.empty()) opc << " " << inc.op1.op;
     if (!inc.op2.op.empty()) opc << ", " << inc.op2.op;
     if (!inc.op3.op.empty()) opc << ", " << inc.op3.op;
     for (const auto &arg : inc.vop) {
         if (!arg.op.empty()) opc << ", " << arg.op;
     }
     if(opc.str().length() > 0) {
-        out << " " << opc.str();
-    }  
+        out << opc.str() << "]";
+    }  else {
+        out << "]";
+    }
+    
     return out;
 }
 
