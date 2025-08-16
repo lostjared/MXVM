ba50d65a2e7993269f1f24e72a183ee246c4fba7
Jared Bruni <jaredbruni@gmail.com>
2025-08-15 20:28:43 -0700

    removed test functions

--- modules/io/io.c
+++ modules/io/io.c
@@ -19,21 +19,3 @@ int64_t seed_random() {
 int64_t rand_number(int64_t size) {
 	return rand()%size;
 }
-
-void probe0(void) { puts("probe0"); }
-
-void probeI8(
-  int64_t a,int64_t b,int64_t c,int64_t d,
-  int64_t e,int64_t f,int64_t g,int64_t h) {
-  printf("I8: %ld %ld %ld %ld %ld %ld %ld %ld\n",
-         a,b,c,d,e,f,g,h);
-}
-
-void probeF5(double a,double b,double c,double d,double e){
-  printf("F5: %.0f %.0f %.0f %.0f %.0f\n", a,b,c,d,e);
-}
-
-void probeMix(
-  int64_t a,double b,int64_t c,double d,int64_t e) {
-  printf("Mix: %ld %.0f %ld %.0f %ld\n", a,b,c,d,e);
-}
