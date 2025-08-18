2932b937d838f80e20051dd702918d18fb8add9b
Jared Bruni <jaredbruni@gmail.com>
2025-08-18 01:04:50 -0700

    standard library module

--- modules/std/mx_std.h
+++ modules/std/mx_std.h
@@ -4,12 +4,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdint.h>
+#include<time.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
+void *mx_memcpy(void *dst, const void *src, size_t n);
+int   mx_memcmp(const void *a, const void *b, size_t n);
+void *mx_memmove(void *dst, const void *src, size_t n);
+void *mx_memset(void *dst, int val, size_t n);
+int64_t mx_time_now_seconds(void);
+int64_t mx_time_now_millis(void);
+int64_t mx_clock_ms(void);
 
 #ifdef __cplusplus
 }
--- modules/std/std.c
+++ modules/std/std.c
@@ -1,2 +1,30 @@
 #include "mx_std.h"
+#include <stddef.h>
+#include <time.h>
+#include <string.h>
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
 
+void *mx_memcpy(void *dst, const void *src, size_t n) {
+    if (!dst || !src || n == 0) return dst;
+    return memcpy(dst, src, n);
+}
+
+int mx_memcmp(const void *a, const void *b, size_t n) {
+    if (n == 0) return 0;
+    return memcmp(a, b, n);
+}
+
+void *mx_memmove(void *dst, const void *src, size_t n) {
+    if (!dst || !src || n == 0) return dst;
+    return memmove(dst, src, n);
+}
+
+void *mx_memset(void *dst, int val, size_t n) {
+    if (!dst || n == 0) return dst;
+    return memset(dst, val, n);
+} 
