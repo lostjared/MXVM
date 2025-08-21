879b515659e886aa6f38ffa55783ae0db7edf0a8
Jared Bruni <jaredbruni@gmail.com>
2025-08-20 18:09:29 -0700

    fibonacci

---
+++ src/frontend/pas/fibonacci.pas
@@ -0,0 +1,25 @@
+program Fibonacci;
+var
+  n: integer;
+  a, b, temp: integer;
+  i: Integer;
+  code: integer;
+begin
+  a := 0;
+  b := 1;
+  i := 2;
+  n := 92;
+  write(a, ' ');
+  if n > 1 then
+    writeln(b, ' ');
+
+  while i < n do
+  begin
+    temp := a + b;
+    a := b;
+    b := temp;
+    writeln(b, ' ');
+    i := i + 1;
+  end;
+  writeln(' done ');
+end.
