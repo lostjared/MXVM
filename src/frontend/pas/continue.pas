5cefeccd05f88b8ed8b188a3f46b1fdea08ab002
Jared Bruni <jaredbruni@gmail.com>
2025-08-30 16:56:44 -0700

    continue

--- src/frontend/pas/continue.pas
+++ src/frontend/pas/continue.pas
@@ -6,7 +6,26 @@ begin
 for i := 0 to 20 do
 begin
 	if i < 10 then continue;
-	writeln('loop index: ', i);
+	writeln('for loop index: ', i);
 end;
 
+i := 0;
+
+while i < 10 do 
+begin
+
+i := i  + 1;
+if i > 5 then continue;
+writeln('while index: ', i)
+
+end;
+
+i := 10;
+
+repeat
+i := i - 1;
+writeln('repeat test: ', i);
+if i = 5 then continue;
+writeln(' no 5: ', i);
+until i = 0;
 end.
