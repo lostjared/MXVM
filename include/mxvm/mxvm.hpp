d404a614805e579f16a7adcfe69b8f483fd0a6a3
Jared Bruni <jaredbruni@gmail.com>
2026-04-05 22:34:43 -0700

    version bump

--- include/mxvm/mxvm.hpp
+++ include/mxvm/mxvm.hpp
@@ -6,5 +6,5 @@
 #ifndef __MXVM__HPP_
 #define __MXVM__HPP_
 #include "icode.hpp"
-#define VERSION_INFO "1.6.0"  ///< MXVM version string
+#define VERSION_INFO "1.6.1"  ///< MXVM version string
 #endif
